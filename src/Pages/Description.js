e3781b7a37cc134684dd2195a0ec1d712dac688a
Omnivers <Twersimor.stock@outlook.com>
2022-08-07 06:28:52 +0200

    Btn watch trailer with no func

--- src/Pages/Description.js
+++ src/Pages/Description.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import failLoad from '../images/failLoad.png'
+import YouTube from "react-youtube";
 
 //Firstable we want to get the ID of the Card so let's console log our ID from card click.
 function Description(){
@@ -7,6 +9,7 @@ function Description(){
     // To avoid a 5 Mo localStorage we will just use the ID from {console.log(useParams())}
     const [movie,setMovie]=useState()
     const selected=useParams()
+    const IMAGE_PATH="https://image.tmdb.org/t/p/w1280"
 
     const fetchData =async()=>{
         const request=await fetch(`https://api.themoviedb.org/3/movie/${selected.id}?api_key=f7e05047484d6ec018591df8216ff84e`)
@@ -22,8 +25,40 @@ function Description(){
     }
     return(
         <>
-    <h1 className="title">DESRIPTION</h1>
-    <button onClick={test}>CLICK ME</button>
+    {!movie?(
+        <h1 className="title">Loading</h1>
+    ):(
+        <div className="description" style={{backgroundImage: `linear-gradient(rgba(0,0,0), rgba(0,0,0,0.3)), url("${IMAGE_PATH}${movie.backdrop_path}")`}}>
+        {!movie.poster_path?(
+            <div className="overlay">
+            <img src={failLoad} className="card-img-top" alt={movie.title} />
+            </div>
+        ):(
+        <div className="overlay">
+        <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} className="card-img-top" alt={movie.title} />
+        <button class="trailer learn-more">
+            <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+            </span>
+            <span class="trailer-text">Watch trailer</span>
+        </button>  
+        </div>
+        )}
+        <h1 className="title">{movie.original_title}</h1>
+        <div className="">
+        {movie.genres.map(genre=>(
+            <button className="genre">{genre.name}</button>
+            ))}
+        </div>
+        <p>{movie.overview}</p>
+        <div className="info">
+        <section className="informations"><span>Statut : </span>{movie.status}</section>
+        <section className="informations"><span>Date of release : </span>{movie.release_date?(movie.release_date):("Unknown")}</section>
+        <section className="informations"><span>Reviews : </span>{movie.vote_average}</section>
+        </div>
+        {/* <button onClick={test}>CLICK ME</button> */}
+        </div>
+    )}
     </>
     )
 }
