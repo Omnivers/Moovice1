cbbc90ed730f2ca97d46ce94da61c4eb17671b3d
Omnivers <Twersimor.stock@outlook.com>
2022-08-07 18:44:06 +0200

    Weekly try to fix the deploy bug

--- src/Pages/Weekly.js
+++ src/Pages/Weekly.js
@@ -4,10 +4,18 @@ import Card from '../components/Card';
 const Weekly = () => {
   const [movies,setMovies]=useState([])
   const API_KEY='f7e05047484d6ec018591df8216ff84e'
-  const toDay=moment().format("YYYY-MM-DD")
-  const  weekAgo=moment().subtract(7, 'd').format("YYYY-MM-DD");
+  const toDay=()=>{
+    const date=moment().format("YYYY-MM-DD")
+    return date;
+  }
+  const weekAgo=()=>{
+    const week=moment().subtract(7, 'd').format("YYYY-MM-DD");
+    return week;
+  }
+  
+  
   const fetchData=async ()=>{
-    const request= await fetch(`http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${weekAgo}&primary_release_date.lte=${toDay}&api_key=${API_KEY}`)
+    const request= await fetch(`http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${weekAgo()}&primary_release_date.lte=${toDay()}&api_key=${API_KEY}`)
     const response= await request.json()
     setMovies(response.results)
   }
